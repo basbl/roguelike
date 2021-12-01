41d31e896c9099a2f2e40575f46e4373cdd37a5f
basbl <basbloembergen@gmail.com>
2021-12-01 21:07:43 +0100

    Add map, player, controls, and graphics

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,58 @@
 #![warn(clippy::pedantic)]
 
 mod map;
+mod map_builder;
+mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::player::*;
 }
 
 use prelude::*;
 
-struct State {}
+struct State {
+    map: Map,
+    player: Player,
+}
 
 impl State {
     fn new() -> Self {
-        State {}
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+
+        State {
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
+        }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print_centered(5, "Hello World!");
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::new()
         .with_title("Roguelike")
+        .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(context, State::new())
