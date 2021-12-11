7f1c5e95d4d5fc40d8314897167ad7bcd7a76a4d
basbl <basbloembergen@gmail.com>
2021-12-11 15:49:29 +0100

    Add enemy spawner

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,13 @@ impl State {
 
         spawn_player(&mut ecs, map_builder.player_start);
 
+        map_builder
+            .rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
+
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
 
