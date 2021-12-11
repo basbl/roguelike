45d4f69e8496faa902c9a0c2e74b85d04e05d5e6
basbl <basbloembergen@gmail.com>
2021-12-11 15:59:17 +0100

    Add collision detection with monsters

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,3 +1,4 @@
+mod collisions;
 mod entity_renderer;
 mod map_renderer;
 mod player_input;
@@ -8,6 +9,7 @@ pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(map_renderer::map_render_system())
+        .add_system(collisions::collisions_system())
         .add_system(entity_renderer::entity_renderer_system())
         .build()
 }
