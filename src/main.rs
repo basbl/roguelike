20e62669b4f036aee02e9e200f2695756fb79f14
basbl <basbloembergen@gmail.com>
2021-12-12 12:14:03 +0100

    Implement turn-based monster movement

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod map;
 mod map_builder;
 mod spawners;
 mod systems;
+mod turn_state;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -22,6 +23,7 @@ mod prelude {
     pub use crate::map_builder::*;
     pub use crate::spawners::*;
     pub use crate::systems::*;
+    pub use crate::turn_state::*;
 }
 
 use prelude::*;
@@ -29,7 +31,9 @@ use prelude::*;
 struct State {
     ecs: World,
     resources: Resources,
-    systems: Schedule,
+    input_systems: Schedule,
+    player_systems: Schedule,
+    monster_systems: Schedule,
 }
 
 impl State {
@@ -50,11 +54,14 @@ impl State {
 
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
+        resources.insert(TurnState::AwaitingInput);
 
         Self {
             ecs,
             resources,
-            systems: build_scheduler(),
+            input_systems: build_input_scheduler(),
+            player_systems: build_player_scheduler(),
+            monster_systems: build_monster_scheduler(),
         }
     }
 }
@@ -66,7 +73,23 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
         self.resources.insert(ctx.key);
-        self.systems.execute(&mut self.ecs, &mut self.resources);
+
+        let current_state = self.resources.get::<TurnState>().unwrap().clone();
+
+        match current_state {
+            TurnState::AwaitingInput => self
+                .input_systems
+                .execute(&mut self.ecs, &mut self.resources),
+            TurnState::PlayerTurn => self
+                .player_systems
+                .execute(&mut self.ecs, &mut self.resources),
+            TurnState::MonsterTurn => self
+                .monster_systems
+                .execute(&mut self.ecs, &mut self.resources),
+        };
+
+        // self.systems.execute(&mut self.ecs, &mut self.resources);
+
         render_draw_buffer(ctx).expect("Render error");
     }
 }
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,15 +1,39 @@
 mod collisions;
+mod end_turn;
 mod entity_renderer;
 mod map_renderer;
 mod player_input;
+mod random_move;
 
 use crate::prelude::*;
 
-pub fn build_scheduler() -> Schedule {
+pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .flush()
         .add_system(map_renderer::map_render_system())
-        .add_system(collisions::collisions_system())
         .add_system(entity_renderer::entity_renderer_system())
+        .build()
+}
+
+pub fn build_player_scheduler() -> Schedule {
+    Schedule::builder()
+        .add_system(collisions::collisions_system())
+        .flush()
+        .add_system(map_renderer::map_render_system())
+        .add_system(entity_renderer::entity_renderer_system())
+        .add_system(end_turn::end_turn_system())
+        .build()
+}
+
+pub fn build_monster_scheduler() -> Schedule {
+    Schedule::builder()
+        .add_system(random_move::random_move_system())
+        .flush()
+        .add_system(collisions::collisions_system())
+        .flush()
+        .add_system(map_renderer::map_render_system())
+        .add_system(entity_renderer::entity_renderer_system())
+        .add_system(end_turn::end_turn_system())
         .build()
 }
