b121e2938d2d59ed891206741895ffa877b21d4a
basbl <basbloembergen@gmail.com>
2021-12-14 21:29:06 +0100

    Add tooltips

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -6,6 +6,7 @@ mod map_renderer;
 mod movement;
 mod player_input;
 mod random_move;
+mod tooltips;
 
 use crate::prelude::*;
 
@@ -15,6 +16,7 @@ pub fn build_input_scheduler() -> Schedule {
         .flush()
         .add_system(map_renderer::map_render_system())
         .add_system(entity_renderer::entity_renderer_system())
+        .add_system(tooltips::tooltips_system())
         .add_system(hud::hud_system())
         .build()
 }
@@ -27,6 +29,7 @@ pub fn build_player_scheduler() -> Schedule {
         .flush()
         .add_system(map_renderer::map_render_system())
         .add_system(entity_renderer::entity_renderer_system())
+        .add_system(tooltips::tooltips_system())
         .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
@@ -42,6 +45,7 @@ pub fn build_monster_scheduler() -> Schedule {
         .flush()
         .add_system(map_renderer::map_render_system())
         .add_system(entity_renderer::entity_renderer_system())
+        .add_system(tooltips::tooltips_system())
         .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
