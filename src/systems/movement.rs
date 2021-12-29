b232b062ec5267b447ccf2b1ac9f4a87f8881cae
basbl <basbloembergen@gmail.com>
2021-12-29 21:30:30 +0100

    Add field of view system

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 
 #[system(for_each)]
 #[read_component(Player)]
+#[read_component(FieldOfView)]
 pub fn movement(
     entity: &Entity,
     want_move: &WantsToMove,
@@ -19,7 +20,15 @@ pub fn movement(
             .get_component::<Player>()
             .is_ok()
         {
-            camera.on_player_move(want_move.destination);
+            if let Ok(entry) = ecs.entry_ref(want_move.entity) {
+                if let Ok(fov) = entry.get_component::<FieldOfView>() {
+                    commands.add_component(want_move.entity, fov.clone_dirty());
+                }
+
+                if entry.get_component::<Player>().is_ok() {
+                    camera.on_player_move(want_move.destination);
+                }
+            }
         }
     }
 
