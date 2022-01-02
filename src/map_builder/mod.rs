2e1b1f7781643deb249065e7bb495fc28674398f
basbl <basbloembergen@gmail.com>
2022-01-02 14:38:42 +0100

    Add cellular automatata map architect

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -1,7 +1,11 @@
 use crate::prelude::*;
+use automata::CellularAutomataArchitect;
 use empty::EmptyArchitect;
+use rooms::RoomsArchitect;
 
+mod automata;
 mod empty;
+mod rooms;
 
 trait MapArchitect {
     fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
@@ -112,22 +116,35 @@ impl MapBuilder {
         }
     }
 
+    fn spawn_monsters(&self, start: &Point, rng: &mut RandomNumberGenerator) -> Vec<Point> {
+        const NUM_MONSTERS: usize = 50;
+
+        let mut spawnable_tiles: Vec<Point> = self
+            .map
+            .tiles
+            .iter()
+            .enumerate()
+            .filter(|(idx, t)| {
+                **t == TileType::Floor
+                    && DistanceAlg::Pythagoras.distance2d(*start, self.map.index_to_point2d(*idx))
+                        > 10.0
+            })
+            .map(|(idx, _)| self.map.index_to_point2d(idx))
+            .collect();
+
+        let mut spawns = Vec::new();
+        for _ in 0..NUM_MONSTERS {
+            let target_index = rng.random_slice_index(&spawnable_tiles).unwrap();
+
+            spawns.push(spawnable_tiles[target_index].clone());
+            spawnable_tiles.remove(target_index);
+        }
+
+        spawns
+    }
+
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = EmptyArchitect {};
+        let mut architect = CellularAutomataArchitect {};
         architect.new(rng)
-        // let mut mb = MapBuilder {
-        //     map: Map::new(),
-        //     rooms: Vec::new(),
-        //     player_start: Point::zero(),
-        //     amulet_start: Point::zero(),
-        // };
-        //
-        // mb.fill(TileType::Wall);
-        // mb.build_random_rooms(rng);
-        // mb.build_corridors(rng);
-        // mb.player_start = mb.rooms[0].center();
-        // mb.amulet_start = mb.find_most_distant();
-        //
-        // mb
     }
 }
